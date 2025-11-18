c50d905be3d0bd91d4f3ce2672429f28c55bb210
sarang-001 <saranguapdhye94@gmail.com>
2025-11-18 16:22:57 +0530

    initial playwright setup

---
+++ tests/example.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('basic test', async ({ page }) => {
+  await page.goto('http://localhost:5173');
+  await expect(page).toHaveTitle(/Todo App/);
+});
+
+test('Add a todo item', async ({ page }) => {
+  await page.goto('/');
+
+  await page.fill('#todo-input', 'Buy milk');
+  await page.click('#add-todo-button');
+
+  const item = await page.locator('.todo-item', { hasText: 'Buy milk' });
+  await expect(item).toBeVisible();
+});
